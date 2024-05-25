c3325591dca47f4c0cade03d35cee25f318c6c4f
guilhermelcosta <guilhermeldcosta@gmail.com>
2024-05-25 16:39:46 -0300

    Finalizado testes da camada de controller

--- todo-list/codigo/src/test/java/pucminas/todolist/controller/TaskControllerImplTest.java
+++ todo-list/codigo/src/test/java/pucminas/todolist/controller/TaskControllerImplTest.java
@@ -1,35 +1,146 @@
 package pucminas.todolist.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.ResultActions;
+import pucminas.todolist.entity.Task;
+import pucminas.todolist.service.interfaces.TaskService;
 
-@SpringBootTest
+import java.util.List;
+
+import static com.jayway.jsonpath.internal.Utils.concat;
+import static java.util.Arrays.asList;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willDoNothing;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static pucminas.todolist.mocks.TaskMock.getTaskMockWithDueDate;
+import static pucminas.todolist.util.TaskConstants.ID_MOCK;
+import static pucminas.todolist.util.constants.Constants.TASK_ENDPOINT;
+import static pucminas.todolist.util.constants.Constants.ZERO;
+
 @ExtendWith(MockitoExtension.class)
+@WebMvcTest
 public class TaskControllerImplTest {
 
-    @InjectMocks
+    @Autowired
     private MockMvc mockMvc;
 
-    public void setup() {
+    @Autowired
+    private ObjectMapper objectMapper;
 
+    @MockBean
+    private TaskService taskService;
+
+    private Task taskMock;
+
+    @BeforeEach
+    public void setUp() {
+        taskMock = getTaskMockWithDueDate();
     }
 
-//    @Test
-//    @DisplayName("findById: success -> return HTTP code 200")
-//    void findById_success_returnHttpCode200() throws Exception {
-//        // Given
-//        mockMvc.perform(MockMvcRequestBuilders.get("https://lab-desenvolvimento.onrender.com/todo-list/task"))
-//                .andExpect(MockMvcResultMatchers.status().isOk());
-//
-//        // When
-//
-//        //Then
-//    }
+    @Test
+    @DisplayName("findById: task found -> return task object")
+    void findById_taskFound_returnTaskObject() throws Exception {
+        // Given
+        given(taskService.findById(ID_MOCK)).willReturn(taskMock);
+
+        // When
+        ResultActions respostaAPI = mockMvc.perform(get(concat("/", TASK_ENDPOINT, "/", ID_MOCK.toString())));
+
+        //Then
+        respostaAPI.andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("findAll: list all tasks -> return task list")
+    void findAll_listAllTasks_returnTaskList() throws Exception {
+        // Given
+        List<Task> tasks = asList(taskMock, taskMock);
+        given(taskService.findAll()).willReturn(tasks);
+
+        // When
+        ResultActions respostaAPI = mockMvc.perform(get(concat("/", TASK_ENDPOINT)));
+
+        //Then
+        respostaAPI.andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("post: task is create -> return created task")
+    void post_taskIsCreated_returnCreatedTask() throws Exception {
+        // Given
+        given(taskService.create(any(Task.class)))
+                .willAnswer((invocation) -> invocation.getArgument(ZERO));
+
+        // When
+        ResultActions respostaAPI = mockMvc.perform(post(concat("/", TASK_ENDPOINT))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(taskMock)));
+
+        //Then
+        respostaAPI.andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("update: task updated -> return task updated")
+    void update_taskUpdated_returnTaskUpdated() throws Exception {
+        // Given
+        given(taskService.update(any(Task.class)))
+                .willAnswer((invocation) -> invocation.getArgument(ZERO));
+
+        // When
+        ResultActions respostaAPI = mockMvc.perform(put(concat("/", TASK_ENDPOINT, "/", ID_MOCK.toString()))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(taskMock)));
+
+        //Then
+        respostaAPI.andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("patch: task updated -> return task updated")
+    void patch_taskUpdated_returnTaskUpdated() throws Exception {
+        // Given
+        given(taskService.updateIsCompletedStatus(ID_MOCK))
+                .willReturn(taskMock);
+
+        // When
+        ResultActions respostaAPI = mockMvc.perform(patch(concat("/", TASK_ENDPOINT, "/{id}"), ID_MOCK.toString())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(taskMock)));
+
+        // Then
+        respostaAPI.andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("delete: task deleted -> task must be deleted")
+    void delete_taskDeleted_taskMustBeDeleted() throws Exception {
+        // Given
+        willDoNothing().given(taskService).delete(ID_MOCK);
+
+        // When
+        ResultActions respostaAPI = mockMvc.perform(delete(concat("/", TASK_ENDPOINT, "/{id}"), ID_MOCK.toString()));
+        //Then
+        respostaAPI
+                .andDo(print())
+                .andExpect(status().isNoContent());
+    }
 }
