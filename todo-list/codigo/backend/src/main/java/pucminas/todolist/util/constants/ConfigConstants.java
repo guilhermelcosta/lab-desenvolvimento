e79abad747c1315045ad04873c0e050fc45b1aba
guilhermelcosta <guilhermeldcosta@gmail.com>
2024-06-21 21:32:23 -0300

    Adicionado DELETE nos metoso CORS

--- todo-list/codigo/backend/src/main/java/pucminas/todolist/util/constants/ConfigConstants.java
+++ todo-list/codigo/backend/src/main/java/pucminas/todolist/util/constants/ConfigConstants.java
@@ -9,7 +9,7 @@ import static java.util.Arrays.asList;
 @UtilityClass
 public class ConfigConstants {
 
-    public static final List<String> ALLOWED_METHODS = asList("GET", "POST", "PUT", "PATCH", "HEAD", "OPTIONS");
+    public static final List<String> ALLOWED_METHODS = asList("GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS");
 
     public static final String CORS_CONFIG = "/**";
 }
